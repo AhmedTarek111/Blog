fd91d4546ed2031af8d4c7b685505ddb4cb692fd
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2023-07-10 20:52:41 +0300

    add images and tags

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-from django.utils import timezone
-
-
+from django.utils import timezone 
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=40000)
-    create_date=models.DateTimeField(default=timezone.now())
+    create_date=models.DateTimeField(default=timezone.now)
     draft=models.BooleanField(default=True)
+    tags=TaggableManager()
+    image=models.ImageField(upload_to='images')
+    # author=
