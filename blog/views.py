02827eccabdcc2ae85618213167bd3707a980956
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2023-07-11 20:07:53 +0300

    add post_details & post_list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    data= Post.objects.all()
+    return render(request,'post_list.html',{'Posts':data})
+
+def all_post_details(request,value):
+    data= Post.objects.get(id=value)
+    return render (request,'post_details.html',{'post_details':data})
--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from blog.views import post_list,all_post_details
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/',post_list),
+    path('blog/<int:value>/',all_post_details),
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
